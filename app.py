38066d3d64d1a551af8681bee23aeaed70dadef5
SeñorZeta <eacrapanzano@gmail.com>
2026-06-06 19:04:09 -0300

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -285,7 +285,6 @@ with tab2:
 st.write("---")
 st.markdown("""
 <div style="background-color: #f4f4f4; padding: 15px; border-radius: 8px; border-left: 5px solid #008a45;">
-    <b>💡 RECORDÁ: Ningún organismo, banco, empresa de servicios, obra social o prepaga te pedirá claves o tokens por mensaje. 
-    Ante cualquier duda contactate a través de los canales oficiales.
+    💡 <b>RECORDÁ:</b> Ningún organismo, banco, empresa de servicios, obra social o prepaga te pedirá claves o tokens por mensaje. Ante cualquier duda, cortá la comunicación y contactate siempre a través de los canales oficiales.
 </div>
 """, unsafe_allow_html=True)
